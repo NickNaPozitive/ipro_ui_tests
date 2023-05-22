a20c538bee17a7511b88dd17cb935f7a0ff137d0
NickNaPozitive <korbofos.nn@gmail.com>
2023-05-22 08:18:59 +0300

    22.05

---
+++ src/main/java/common/CommonActions.java
@@ -0,0 +1,35 @@
+package common;
+
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+import static common.Config.BROWSER_AND_PLATFORM;
+import static constants.Constant.TimeOutVariables.IMPLICIT_WAIT;
+
+
+public class CommonActions extends Assert {
+
+    public static WebDriver createDriver(){
+        WebDriver driver = null;
+        switch (BROWSER_AND_PLATFORM){
+            case "CHROME_WIN":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+                driver = new ChromeDriver();
+                break;
+            case "MOZILLA":
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                assertEquals(BROWSER_AND_PLATFORM, "qwe", "hello");
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+        return driver;
+    }
+}
